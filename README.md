a776db7a0a94033899dc85289a5957cf564b5be7
Crag <ogeidca49@gmail.com>
2023-04-18 15:29:13 -0600

    change code, delete external libraries

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ Plugin collection to integrate with catapult launcher
 Just write the word tr, then the base language and the
 language to translate, finally just add what you want translate.
 
-Remember that for the translation to be executed you must add a blank space at the end.
+Remember that for the translation to be executed you must add a trailing whitespace.
 
-Format: tr <source language> <target language> <Text to translate>
+Format: tr <source language> <target language> <Text to translate><Add a trailing whitespace to translate>
 Example: tr en ru Hello World
 
 ### Demo
@@ -23,7 +23,6 @@ Example: tr en ru Hello World
 Installation:
 
 ```bash
-pip install aiohttp
 curl https://raw.githubusercontent.com/CRAG666/catapult-plugins-collection/main/plugins/translator.py --output ~/.local/share/catapult/plugins/translator.py
 ```
 
@@ -38,20 +37,20 @@ Exaple: sha256 Hello World!!
 
 ### Available methods
 
- * blake2b
- * blake2s
- * md5
- * sha1
- * sha224
- * sha256
- * sha384
- * sha3224
- * sha3256
- * sha3384
- * sha3512
- * sha512
- * shake128
- * shake256
+- blake2b
+- blake2s
+- md5
+- sha1
+- sha224
+- sha256
+- sha384
+- sha3224
+- sha3256
+- sha3384
+- sha3512
+- sha512
+- shake128
+- shake256
 
 ### Demo
 
--- plugins/translator.py
+++ plugins/translator.py
@@ -1,8 +1,10 @@
-import asyncio
 import hashlib
+import http.client
+import json
 import re
+import urllib.parse
+import urllib.request
 
-import aiohttp
 # import requests
 from catapult.api import (Plugin, SearchResult, copy_text_to_clipboard,
                           lookup_icon)
@@ -10,7 +12,7 @@ from catapult.i18n import _
 
 cache = {}
 
-PATTERN = r"^tr\s([a-z]{2})\s([a-z]{2})\s(.*)\s"
+PATTERN = re.compile(r"^tr\s([a-z]{2})\s([a-z]{2})\s(.*)\s")
 
 
 class TranslatorPlugin(Plugin):
@@ -20,18 +22,23 @@ class TranslatorPlugin(Plugin):
 
     def __init__(self):
         super().__init__()
+        self.url = "/translate_a/single"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": "https://translate.google.com/",
+            "Origin": "https://translate.google.com",
+        }
+        self.conn = http.client.HTTPSConnection("translate.googleapis.com")
 
-    def launch(self, window, id):
-        copy_text_to_clipboard(cache.get(self.cache_key, ""))
-
-    async def google_translate(self, text, source_language, target_language):
+    def google_translate(self, text, source_language, target_language):
         self.cache_key = hashlib.md5(
             f"{text}{source_language}{target_language}".encode()
         ).hexdigest()
         if self.cache_key in cache:
             return cache[self.cache_key]
 
-        url = "https://translate.googleapis.com/translate_a/single"
         params = {
             "client": "gtx",
             "sl": source_language,
@@ -39,14 +46,28 @@ class TranslatorPlugin(Plugin):
             "dt": "t",
             "q": text,
         }
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-        }
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(url, params=params, timeout=3) as response:
-                result = (await response.json())[0][0][0]
-                cache[self.cache_key] = result
-        return result
+
+        data = urllib.parse.urlencode(params).encode("utf-8")
+
+        self.conn.request("POST", self.url, data, self.headers)
+
+        response = self.conn.getresponse()
+
+        result_text = response.read().decode()
+
+        # self.conn.close()
+
+        try:
+            result = json.loads(result_text)
+            result_text = result[0][0][0]
+        except (json.JSONDecodeError, IndexError):
+            result_text = ""
+
+        cache[self.cache_key] = result_text
+        return result_text
+
+    def launch(self, window, id):
+        copy_text_to_clipboard(cache.get(self.cache_key, ""))
 
     def search(self, query):
         match = re.match(PATTERN, query)
@@ -54,7 +75,7 @@ class TranslatorPlugin(Plugin):
             return
 
         src, dest, text = match.groups()
-        result = asyncio.run(self.google_translate(text, src, dest))
+        result = self.google_translate(text, src, dest)
         if result:
             yield SearchResult(
                 description=result,
@@ -66,9 +87,9 @@ class TranslatorPlugin(Plugin):
                     "org.gnome.Translate",
                     "application-x-executable",
                 ),
-                id="",
+                id="0",
                 offset=0,
                 plugin=self,
                 score=1,
-                title="Translation",
+                title="Traduccion",
             )
